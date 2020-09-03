de1a9055774e95fdfe380a878263e1dd6b1be13f
MarkSmith1997 <mark.smith2010@hotmail.co.uk>
2020-09-03 15:20:03 +0100

    implement the shunting yard algorithm for a given string

---
+++ Calculator1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Calculator1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] operators = { "+", "-", "*", "/", "^"};
+            Dictionary<string, int> precedence = new Dictionary<string, int>(){ {"+", 2 }, {"-", 2}, {"*", 3}, {"/", 3}, {"^", 4} };
+            Queue<string> outputQueue = new Queue<string>();
+            Stack<string> operatorStack = new Stack<string>();
+            string input = Console.ReadLine();
+            string[] inputQueue = input.Split();
+
+            foreach(string s in inputQueue)
+            {
+                if (operators.Contains(s))
+                {
+                    while(operatorStack.Count > 0 && precedence[operatorStack.Peek()] > precedence[s])
+                    {
+                        outputQueue.Enqueue(operatorStack.Pop());
+                    }
+                    operatorStack.Push(s);
+                }
+                else
+                {
+                    outputQueue.Enqueue(s);
+                }
+            }
+            while (operatorStack.Count > 0)
+            {
+                outputQueue.Enqueue(operatorStack.Pop());
+            }
+
+        }
+    }
+}
