560d97d28b0d77a8fe4f1c0b6f0e35e1571370fa
MarkSmith1997 <mark.smith2010@hotmail.co.uk>
2020-09-09 17:55:40 +0100

    add parenthesis to parsing

--- Calculator1/Program.cs
+++ Calculator1/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Xml;
 using System.Xml.Serialization;
+using System.Xml.XPath;
 
 namespace Calculator1
 {
@@ -12,17 +13,45 @@ namespace Calculator1
         static void Main()
         {
             string[] operators = { "+", "-", "*", "/", "^"};
-            Dictionary<string, int> precedence = new Dictionary<string, int>(){ {"+", 2}, {"-", 2}, {"*", 3}, {"/", 3}, {"^", 4} };
+            Dictionary<string, int> precedence = new Dictionary<string, int>(){ {"+", 2}, {"-", 2}, {"*", 3}, {"/", 3}, {"^", 4}, {"(", 0}, {")", 5} };
             Queue<string> outputQueue = new Queue<string>();
             Stack<string> operatorStack = new Stack<string>();
+            Console.WriteLine("Enter expression:");
             string input = Console.ReadLine();
             string[] inputQueue = input.Split();
+            bool parenthesisFound;
 
             foreach(string s in inputQueue)
             {
-                if (operators.Contains(s))
+                if(s == "(")
                 {
-                    while(operatorStack.Count > 0 && precedence[operatorStack.Peek()] > precedence[s])
+                    operatorStack.Push(s);
+                }
+                else if (s == ")")
+                {
+                    parenthesisFound = true;
+                    while (parenthesisFound)
+                    {
+                        if(operatorStack.Count == 0)
+                        {
+                            parenthesisFound = false;
+                            Console.WriteLine("Error: Parenthesis mismatch, revise input.");
+                            break;
+                        }
+                        if (operatorStack.Peek() == "(")
+                        {
+                            operatorStack.Pop();
+                            parenthesisFound = false;
+                        }
+                        else
+                        {
+                            outputQueue.Enqueue(operatorStack.Pop());
+                        }
+                    }
+                }
+                else if (operators.Contains(s))
+                {
+                    while (operatorStack.Count > 0 && precedence[operatorStack.Peek()] > precedence[s])
                     {
                         outputQueue.Enqueue(operatorStack.Pop());
                     }
@@ -35,6 +64,11 @@ namespace Calculator1
             }
             while (operatorStack.Count > 0)
             {
+                if(operatorStack.Peek() == "(")
+                {
+                    Console.WriteLine("Error: Parenthesis mismatch, revise input.");
+                    break;
+                }                   
                 outputQueue.Enqueue(operatorStack.Pop());
             }
 
@@ -44,7 +78,6 @@ namespace Calculator1
                 printString += outputQueue.Dequeue() + " ";
             }
             Console.WriteLine(printString);
-
         }
     }
 }
